3bff218b834899e37d73f4a8d2f36863de27d0f3
waro163 <waro163@163.com>
2020-04-29 16:05:57 +0800

    add remote_user for django_rest_framework

--- djangorestframework_jwt_authentication/user.py
+++ djangorestframework_jwt_authentication/user.py
@@ -13,3 +13,20 @@ class User(BaseUser):
             raise
         for key, value in payload.items():
             setattr(self, key, value)
+
+class RemoteUser(object):
+
+    # This property is for drf RemoteUser authenticate
+    @property
+    def is_active(self):
+        return True
+
+    def authenticate(self, request, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        return self
+
+    # This property is for drf check permission
+    @property
+    def is_authenticated(self):
+        return True
